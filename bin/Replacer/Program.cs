c12a14178aacc0da6533aeb41f04cc5e5e0cbca4
Ivan Puzatkin <puzatkin1998@gmail.com>
2020-05-29 15:32:51 +0300

    Небольшой лайфхак

---
+++ bin/Replacer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace Replacer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextReader reader = new StreamReader("../../out/lsdlang.il");
+
+            string result = "";
+            while (reader.Peek() > -1)
+            {
+                result += reader.ReadLine().Replace("double", "float64") + "\n";
+            }
+
+            StreamWriter writer = new StreamWriter("../../out/lsdlang.il");
+
+            writer.WriteLine(result);
+            writer.Flush();
+            writer.Close();
+        }
+    }
+}
